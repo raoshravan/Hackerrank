ad6934719b64d509916697eb3f3d5f1fb8c2c2d8
raoshravan <raoshravan22@gmail.com>
2019-03-27 23:21:23 +0530

    Create MigratoryBirds.cs

---
+++ MigratoryBirds.cs
@@ -0,0 +1,37 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the migratoryBirds function below.
+    static int migratoryBirds(List<int> arr) {
+                Dictionary<int,int> keyvaluepair=arr.GroupBy(item => item).ToDictionary( key => key.Key,value => value.Count()) ;
+            return keyvaluepair.Where(item => item.Value == keyvaluepair.Values.Max()).Select( element => element.Key).Min();
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = migratoryBirds(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
