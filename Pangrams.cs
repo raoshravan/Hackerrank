ec1cd197cb9a68d10c2651c4e94b73bc116525ba
raoshravan <raoshravan22@gmail.com>
2019-03-27 22:54:46 +0530

    Create Pangrams.cs

---
+++ Pangrams.cs
@@ -0,0 +1,35 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the pangrams function below.
+    static string pangrams(string s) {
+return s.Replace(" ", string.Empty).ToLower().ToCharArray().GroupBy(element => element).Count() == 26 ? "pangram" : "not pangram";
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = pangrams(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
